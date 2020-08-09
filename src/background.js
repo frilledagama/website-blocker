f7cd0cbf2f071695b02801434713a8b92bdffa0a
frilledagama <thatbiiih@gmail.com>
2020-08-09 18:28:38 -0400

    Add redirectUrl to webRequest Listener

--- src/background.js
+++ src/background.js
@@ -2,17 +2,25 @@
  * backgorund file contains listeners for browser events that are important to the extension
  * It lies dormant until an event is fired then performs the instructed logic. 
  * An effective background script is only loaded when it is needed and unloaded when it goes idle.
+ * @author Maya Scott-Lourenco <maya.scottlourenco@gmail.com>
  **/
-var networkFilters = {
-    urls: [
-        "*://www.facebook.com/*"
-    ]
-}
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details){
 
-    },            // function
-    filter,               //  object
-    ["blocking"]         //  optional array of strings
+//callback functions for Listener event
+
+var requestFilter = {
+    urls: [ "*://www.facebook.com/*" ]
+}, 
+
+extraInfoSpec = ['blocking'],
+
+callback = function(details){
+    //var blockResponse = chrome.webRequest.BlockingResponse;
+    return {redirectUrl: "https://www.youtube.com/watch?v=r9lpLm7jwQY"};
+};
+
+chrome.webRequest.onBeforeRequest.addListener(
+    callback,            
+    requestFilter,
+    extraInfoSpec
 );
 
